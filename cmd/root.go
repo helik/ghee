40388ccbdb3a3b5ff61ba77be7e8c6fa8cfd07b5
Sidney Kochman <sidney@kochman.org>
2017-07-26 13:48:56 -0400

    Remove root run

--- cmd/root.go
+++ cmd/root.go
@@ -1,17 +1,12 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 )
 
 var RootCmd = &cobra.Command{
 	Use:   "ghee",
 	Short: "Simple Kubernetes multi-cluster controller",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("root")
-	},
 }
 
 func Execute() {
