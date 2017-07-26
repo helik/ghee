b83693b6bf994d7660acde7325ec173988e97649
Sidney Kochman <sidney@kochman.org>
2017-07-26 14:32:30 -0400

    Create command reads Gheefile

--- cmd/create.go
+++ cmd/create.go
@@ -1,14 +1,17 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/spf13/cobra"
+
+	"github.com/helik/ghee/controller"
 )
 
 // createCmd represents the create command
 var createCmd = &cobra.Command{
-	Use:   "create",
+	Use:   "create [path to Gheefile]",
 	Short: "Create a resource",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -16,8 +19,20 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
+	PreRunE: func(cmd *cobra.Command, args []string) error {
+		if len(args) < 1 {
+			return errors.New("Path to Gheefile not provided.")
+		}
+		return nil
+	},
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("create called")
+		path := args[0]
+		gheefile, err := controller.ReadGheefile(path)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Printf("%+v", gheefile)
 	},
 }
 
---
+++ controller/manifest.go
@@ -0,0 +1,28 @@
+package controller
+
+import (
+	"io/ioutil"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Gheefile struct {
+	Manifest interface{}
+	Clusters []string
+	Replicas map[string]int
+}
+
+func ReadGheefile(filepath string) (Gheefile, error) {
+	g := Gheefile{}
+	body, err := ioutil.ReadFile(filepath)
+	if err != nil {
+		return g, err
+	}
+
+	err = yaml.Unmarshal(body, &g)
+	if err != nil {
+		return g, err
+	}
+
+	return g, nil
+}
